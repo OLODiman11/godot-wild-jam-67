128652db293f91c6792c760a3319383ddaa7a970
Mark Kirillov <mark.kirilka@mail.ru>
2024-03-14 16:05:29 +0300

    Menu | cutscene | bootstrap

--- scripts/bootstrap.gd
+++ scripts/bootstrap.gd
@@ -1,6 +1,10 @@
 extends Node
 
 @export var current_ally: Ally
+@export var allies_container: Node2D 
+@export var enemies_container: Node2D 
 
 func _ready():
 	PlayerController.character = current_ally
+	PlayerController.allies_container = allies_container
+	PlayerController.enemies_container = enemies_container 
