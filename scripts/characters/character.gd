0f6a2995a9ef13c128d43492e30dc543cc3f8cd0
OLODiman11 <OLODiman11@mail.ru>
2024-03-14 21:13:36 +0300

    Moved health bar show/hide logic to character

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -12,11 +12,20 @@ signal died
 @onready var health_bar: HealthBar = $HealthBar
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var health: Health = $Health
+@onready var health_bar_timer: Timer = $HealthBar/Timer
 
 func _ready():
+	# Die logic
 	health.value_changed.connect(_die_if_zero_health)
+	
+	# Sprite tint logic
 	health.value_changed.connect(_change_sprite_tint)
 	health.max_value_changed.connect(_change_sprite_tint)
+	
+	# Health bar show/hide logic
+	health_bar_timer.timeout.connect(health_bar.hide)
+	health.value_changed.connect(_show_health_bar_and_restart_timer)
+	health.max_value_changed.connect(_show_health_bar_and_restart_timer)
 	
 func _die_if_zero_health(sender: Health):
 	if health.value <= 0:
@@ -28,3 +37,7 @@ func _change_sprite_tint(sender: Health):
 	var factor = health.value / health.max_value
 	var tint = Color(1, factor, factor)
 	sprite_2d.modulate = tint
+	
+func _show_health_bar_and_restart_timer():
+	health_bar.show()
+	health_bar_timer.start()
--- scripts/ui/health_bar.gd
+++ scripts/ui/health_bar.gd
@@ -2,25 +2,17 @@ class_name HealthBar
 
 extends TextureProgressBar
 
-@export var health: Health
+@export var health: Health : set = set_health_component
 
-@onready var timer: Timer = $Timer
-
-func _ready():
-	if health:
-		health.value_changed.connect(func(_x): update())
-		health.max_value_changed.connect(func(_x): update())
-		value = health.value / health.max_value * max_value
-	timer.timeout.connect(hide_bar)
-
-func update():
-	show_bar()
+func update(sender: Health):
 	value = health.value / health.max_value * max_value
-
-func show_bar():
-	timer.start()
-	show()
 	
-func hide_bar():
-	timer.stop()
-	hide()
+func set_health_component(value: Health):
+	if health:
+		health.value_changed.disconnect(update)
+		health.max_value_changed.disconnect(update)
+		
+	health = value
+	health.value_changed.connect(update)
+	health.max_value_changed.connect(update)
+	
