42807ea730df077f8345cdc04090a3f06bcee321
OLODiman11 <OLODiman11@mail.ru>
2024-03-15 01:57:02 +0300

    Added global Resources class with preloaded resources

---
+++ scripts/globals/resources.gd
@@ -0,0 +1,6 @@
+class_name Resources
+
+const CHARACTER_RES: Dictionary = {
+	Character.Type.MELEE: preload("res://resources/enemies/melee.tres"),
+	Character.Type.GUARD: preload("res://resources/enemies/guard.tres")
+}
