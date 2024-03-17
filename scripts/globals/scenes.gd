3a77317fcd10cecab96d712fa2d4967b42ef1c5b
OLODiman11 <OLODiman11@mail.ru>
2024-03-17 03:37:21 +0300

    Added global scenes.gd to preload scenes

---
+++ scripts/globals/scenes.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const MAIN_MENU: PackedScene = preload("res://scenes/menu.tscn")
+const MENU_SETTINGS: PackedScene = preload("res://scenes/ui/menu_settings.tscn")
+const CUTSCENE: PackedScene = preload("res://scenes/cutscene.tscn")
