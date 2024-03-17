4ac7bb0d3653c52389857e48d341d4685fe388f7
OLODiman11 <OLODiman11@mail.ru>
2024-03-17 08:15:41 +0300

    Added _spawn_queue to hold enemies to spawn

--- scripts/wave_manager.gd
+++ scripts/wave_manager.gd
@@ -15,9 +15,9 @@ const WAVES_DIR: String = "res://resources/waves/"
 @export var preload_enemy = preload("res://scenes/characters/enemy.tscn")
 
 var round_num = 0
-var _enemies_to_spawn: int
 
 var _waves: Array[WaveCongif]
+var _spawn_queue: Array[EnemyRes]
 
 func _ready():
 	var wave_file_names = DirAccess.get_files_at(WAVES_DIR)
@@ -27,34 +27,24 @@ func _ready():
 	timer.timeout.connect(spawn_enemy)
 
 func start_next_wave():
-	_enemies_to_spawn = wave_list[round_num]
+	var wave: WaveCongif = _waves[round_num]
+	_populate_spawn_queue(wave)
 	round_num += 1
 	timer.start()
 	wave_started.emit()
 	
 func spawn_enemy():
-	if _enemies_to_spawn == 0:
+	if _spawn_queue.size() == 0:
 		timer.stop()
 		return
 	
-	_enemies_to_spawn -= 1
-	
 	var enemy: Enemy = preload_enemy.instantiate()
-	var chance = randi_range(0, 10)
-	if chance == 0:
-		enemy.enemy_res = load("res://resources/enemies/grenader.tres")
-	elif chance in range(1,3):
-		enemy.enemy_res = load("res://resources/enemies/pistol.tres")
-	elif chance in range(4,5):
-		enemy.enemy_res = load("res://resources/enemies/sniper.tres")
-	elif chance in range(6,7):
-		enemy.enemy_res = load("res://resources/enemies/rifleman.tres")
-	else:
-		enemy.enemy_res = load("res://resources/enemies/melee.tres")
+	
+	enemy.enemy_res = _spawn_queue.pop_back()
+	
 	enemy.get_node("Health").died.connect(func(_x): try_end_wave())
 	enemy.get_node("Health").died.connect(Ally._on_enemy_killed)
 	enemy.converted_to_parasite.connect(try_end_wave)
-	enemy.get_node("AiController").target = PlayerController.instance.character
 	
 	while true:
 		enemy.global_position = _get_enemy_random_position()
@@ -62,11 +52,11 @@ func spawn_enemy():
 		var is_in_bounds_y: bool = abs(enemy.global_position.y) <= MAP_BOUNDS
 		if is_in_bounds_x and is_in_bounds_y:
 			break
-	
+			
 	enemies_container.add_child(enemy)
 		
 func try_end_wave():
-	if _enemies_to_spawn > 0:
+	if _spawn_queue.size() > 0:
 		return
 		
 	for enemy in enemies_container.get_children():
@@ -74,10 +64,20 @@ func try_end_wave():
 			return
 	wave_ended.emit()
 		
-
 func _get_enemy_random_position():
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	var path_follow = spawn_path.get_node("PathFollow2D")
 	path_follow.progress = rng.randi_range(0, 1600)
 	return path_follow.get_node("Marker2D").global_position
+	
+func _populate_spawn_queue(wave: WaveCongif):
+	_spawn_queue.clear()
+	for i in range(wave.enemies.size()):
+		var enemy_res = wave.enemies[i]
+		var amount = wave.amounts[i]
+		var batch: Array[EnemyRes] = []
+		batch.resize(amount)
+		batch.fill(enemy_res)
+		_spawn_queue.append_array(batch)
+	_spawn_queue.shuffle()
