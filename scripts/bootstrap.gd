3923bb91a7971a97c6ebfa9b4b8e2bf2c340602a
OLODiman11 <OLODiman11@mail.ru>
2024-03-17 20:40:21 +0300

    Globals reset

--- scripts/bootstrap.gd
+++ scripts/bootstrap.gd
@@ -5,6 +5,15 @@ extends Node
 @export var enemies_container: Node2D 
 
 func _ready():
+	Globals.upgrade_points = 0
+	Globals.hide_on_esc = false
+	Globals.mother_points = 0
+	
+	PlayerStats.max_health = 500
+	PlayerStats.regen_rate = 1
+	PlayerStats.speed = 800
+	PlayerStats.max_parasite_count = 1
+	
 	PlayerController.instance.character = current_ally
 	PlayerController.instance.allies_container = allies_container
 	PlayerController.instance.enemies_container = enemies_container 
