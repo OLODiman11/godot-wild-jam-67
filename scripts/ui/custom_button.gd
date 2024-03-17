9989f94f38c2677285e8fc02450f5c941d68278e
Mark Kirillov <mark.kirilka@mail.ru>
2024-03-17 12:24:27 +0300

    Added focus on button

--- scripts/ui/custom_button.gd
+++ scripts/ui/custom_button.gd
@@ -4,4 +4,7 @@ extends Button
 
 func _ready():
 	mouse_entered.connect(UiSfxPlayer.button_hover_player.play)
+	focus_entered.connect(UiSfxPlayer.button_hover_player.play)
 	pressed.connect(UiSfxPlayer.button_press_player.play)
+
+
